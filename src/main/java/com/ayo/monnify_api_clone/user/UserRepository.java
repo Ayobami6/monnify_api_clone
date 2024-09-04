5cad0e30703cb4fd42b98f42b5e338684cfff8a0
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-09-04 17:15:52 +0100

    add find merchant by contractcode repo method

--- src/main/java/com/ayo/monnify_api_clone/user/UserRepository.java
+++ src/main/java/com/ayo/monnify_api_clone/user/UserRepository.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
 
     Optional<UserEntity>findByEmail(String email);
     Optional<UserEntity>findByApiKey(String apiKey);
+    Optional<UserEntity>findByContractCode(Long contractCode);
     
 }
